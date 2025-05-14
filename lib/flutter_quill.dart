6075852deec86df1aa557dfc608000e9d876b6cf
Jonathan Weber <jonathan.weber@lyth.de>
2025-05-14 13:33:50 +0200

    small export change

--- lib/flutter_quill.dart
+++ lib/flutter_quill.dart
@@ -34,6 +34,7 @@ export 'src/editor_toolbar_controller_shared/copy_cut_service/copy_cut_service.d
 export 'src/editor_toolbar_controller_shared/copy_cut_service/copy_cut_service_provider.dart';
 export 'src/editor_toolbar_controller_shared/copy_cut_service/default_copy_cut_service.dart';
 export 'src/editor_toolbar_controller_shared/quill_config.dart';
+export 'src/editor_toolbar_shared/color.dart';
 export 'src/editor_toolbar_shared/config/quill_action_configuration.dart';
 export 'src/l10n/generated/quill_localizations.dart';
 export 'src/rules/rule.dart' show Rule;
